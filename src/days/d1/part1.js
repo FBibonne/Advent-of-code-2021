f321fab3809e6c88f19e421c90d068c4a194438f
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-12-07 13:39:10 +0100

    day 1 - short

--- src/days/d1/part1.js
+++ src/days/d1/part1.js
@@ -2,9 +2,9 @@ import { input } from "./input";
 import { d1Extractor } from "./common";
 
 const D1P1 = (input) =>
-  d1Extractor(input).reduce((acc, v, i, array) => {
-    if (i === 0) return acc;
-    return v > array[i - 1] ? acc + 1 : acc;
-  }, 0);
+  d1Extractor(input).reduce(
+    (prev, current, i, tableau)=>i===0?0:(current>tableau[i-1])+prev
+    ,0
+  );
 
-export default D1P1(input);
+  export default D1P1(input);
--- src/days/d1/part2.js
+++ src/days/d1/part2.js
@@ -1,18 +1,12 @@
 import { input } from "./input";
 import { d1Extractor } from "./common";
 
-const D1P2 = (input) => {
-  const inputArray = d1Extractor(input);
-  const inputArrayLength = inputArray.length;
-  return inputArray
-    .reduce((acc, v, i, array) => {
-      if (i > inputArrayLength - 3) return acc;
-      return [...acc, v + array[i + 1] + array[i + 2]];
-    }, [])
-    .reduce((acc, v, i, array) => {
-      if (i === 0) return acc;
-      return v > array[i - 1] ? acc + 1 : acc;
-    }, 0);
-};
+const D1P2 = (input) => 
+  d1Extractor(input).map((value, i, tableau)=> (i===0 || i===tableau.length-1)?null:tableau[i-1]+value+tableau[i+1])
+  .filter(value=>value!=null)
+  .reduce(
+    (prev, current, i, tableau)=>i===0?0:(current>tableau[i-1])+prev
+    ,0
+  );
 
 export default D1P2(input);

03d133ac699bea4c5026c70a3f2aa30f784186b0
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-12-08 06:06:12 +0100

    Needing a for loop !

--- src/days/d1/part2.js
+++ src/days/d1/part2.js
@@ -1,12 +1,18 @@
 import { input } from "./input";
 import { d1Extractor } from "./common";
 
-const D1P2 = (input) => 
-  d1Extractor(input).map((value, i, tableau)=> (i===0 || i===tableau.length-1)?null:tableau[i-1]+value+tableau[i+1])
-  .filter(value=>value!=null)
-  .reduce(
-    (prev, current, i, tableau)=>i===0?0:(current>tableau[i-1])+prev
+const D1P2 = (input) => {
+  const tabInput=d1Extractor(input);
+  const pt= Array(tabInput.length-2);
+  const mapper = (_,i)=>tabInput[i]+tabInput[i+1]+tabInput[i+2];
+  for (let index = 0; index < pt.length; index++) {
+    pt[index]=mapper(pt[index], index);
+  }
+  // Not working ! pt.map(mapper);
+  return pt.reduce(
+    (prev, current, i)=>i===0?0:(current>pt[i-1])+prev
     ,0
   );
+  }
 
 export default D1P2(input);

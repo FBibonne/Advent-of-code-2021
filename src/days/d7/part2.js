cebcfb649ebab7935d25741edaee83dbbc815448
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-12-13 15:16:25 +0100

    day 7

---
+++ src/days/d7/part2.js
@@ -0,0 +1,34 @@
+import { input } from "./input";
+import { d6Extractor} from "../d6/common";
+
+const D7P2 = (input) => {
+    const positions=d6Extractor(input);
+    const {min, max}=findMinMax(positions);
+    var bestConsumption=calcConsumption(positions, min);
+    for (let align = min+1; align <=max ; align++) {
+        const newConsumption=calcConsumption(positions, align);
+        if (newConsumption<bestConsumption){
+            bestConsumption=newConsumption;
+        }
+    }
+    return bestConsumption;
+}
+
+const calcConsumption=(positions, align)=>positions.reduce((total, current)=>total+(Math.abs(current-align))*(Math.abs(current-align)+1)/2,0);
+
+const findMinMax=array=>{
+    var min=array[0];
+    var max=array[0];
+    if (array.length>1){
+        for (const value of array) {
+            if(value<min){
+                min=value;
+            }
+            if(value > max){
+                max=value;
+            }
+        }
+    }
+    return {min, max};
+}
+export default D7P2(input);
